8a52eee7e6e65217604336671fccc6a8175c6324
Sukuna <sukuna@velhaturbo.cloud>
2026-06-14 00:50:19 -0300

    fix(data.js): retorna data URI em vez de SVG cru nas figurinhas

--- data.js
+++ data.js
@@ -352,7 +352,7 @@
     const tipo = bandeira ? bandeira.tipo : 'faixas-h';
     const inner = gerarEscudo(cores, tipo);
 
-    return `
+    const svg = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 280" width="200" height="280">
   <defs>
     <linearGradient id="bg-${fig.id}" x1="0" y1="0" x2="0" y2="1">
